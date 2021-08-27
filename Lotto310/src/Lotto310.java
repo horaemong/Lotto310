5ae25b87fc0ab93e35056e551cc12d0533547625
horaemong <69315573+horaemong@users.noreply.github.com>
2021-08-27 15:12:49 +0900

    Add files via upload

---
+++ Lotto310/src/Lotto310.java
@@ -0,0 +1,21 @@
+
+public class Lotto310  extends JFrame implements ActionListener{
+	JLabel firstLabel;
+	JLabel secondLabel;
+	JLabel thirdLabel;
+	JButton lottery;
+	JButton reset;
+	JButton close;
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
